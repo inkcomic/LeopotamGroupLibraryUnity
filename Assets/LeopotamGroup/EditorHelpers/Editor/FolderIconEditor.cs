1838dcb6ea74c83effa516b1e9fb0b7fb0e6a8d3
Leopotam <leopotam@gmail.com>
2016-10-29 16:23:09 +0300

    [+] EditorHelpers: FolderIconEditor - visual customization of project folders.

---
+++ Assets/LeopotamGroup/EditorHelpers/Editor/FolderIconEditor.cs
@@ -0,0 +1,189 @@
+﻿//-------------------------------------------------------
+// LeopotamGroupLibrary for unity3d
+// Copyright (c) 2012-2016 Leopotam <leopotam@gmail.com>
+//-------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using LeopotamGroup.Common;
+using LeopotamGroup.Serialization;
+using UnityEditor;
+using UnityEngine;
+
+namespace LeopotamGroup.EditorHelpers.UnityEditors {
+    sealed class FolderIconEditor : EditorWindow {
+        class FolderIconDesc {
+            Color32? _validColor;
+
+            [JsonIgnore]
+            public Color32 ValidColor {
+                get {
+                    if (!_validColor.HasValue) {
+                        _validColor = HexColor.ToColor24 ();
+                    }
+                    return _validColor.Value;
+                }
+                set {
+                    _validColor = value;
+                    HexColor = ((Color) value).ToHexString24 ();
+                }
+            }
+
+            [JsonName ("c")]
+            public string HexColor = "ffffff";
+
+            [JsonName ("i")]
+            public string OverlayIcon;
+        }
+
+        const string Title = "Folder Icon Editor";
+
+        const int IconSize = 64;
+
+        const string FolderIconName = "Folder Icon";
+
+        const string StorageKey = "lg.folder-icon.storage";
+
+        static Texture2D _folderIconBack;
+
+        static Dictionary<string, FolderIconDesc> _allDescs;
+
+        string _folderPath;
+
+        string _folderGuid;
+
+        FolderIconDesc _folderDesc;
+
+        [InitializeOnLoadMethod]
+        static void InitFolderIconEditor () {
+            EditorApplication.projectWindowItemOnGUI += OnDrawProjectWindowItem;
+            if ((System.Object) _folderIconBack == null) {
+                _folderIconBack = EditorGUIUtility.Load (FolderIconName) as Texture2D;
+            }
+            LoadInfo ();
+            SaveInfo ();
+        }
+
+        static void LoadInfo () {
+            try {
+                _allDescs = JsonSerialization.DeserializeStatic<Dictionary<string, FolderIconDesc>> (ProjectPrefs.GetString (StorageKey, "{}"));
+                if (_allDescs == null) {
+                    throw new Exception ();
+                }
+            } catch {
+                _allDescs = new Dictionary<string, FolderIconDesc> ();
+            }
+        }
+
+        static void SaveInfo () {
+            if (_allDescs.Count > 0) {
+                try {
+                    ProjectPrefs.SetString (StorageKey, JsonSerialization.SerializeStatic (_allDescs));
+                } catch {
+                }
+            } else {
+                ProjectPrefs.DeleteKey (StorageKey);
+            }
+        }
+
+        static void OnDrawProjectWindowItem (string guid, Rect rect) {
+            var path = AssetDatabase.GUIDToAssetPath (guid);
+            if (AssetDatabase.IsValidFolder (path)) {
+                var icon = GetCustomIcon (guid);
+                if (icon != null) {
+                    if (rect.width > rect.height) {
+                        rect.width = rect.height;
+                    } else {
+                        rect.height = rect.width;
+                    }
+                    if (rect.width > IconSize) {
+                        var offset = (rect.width - IconSize) * 0.5f;
+                        rect.Set (rect.x + offset, rect.y + offset, IconSize, IconSize);
+                    }
+                    var savedColor = GUI.color;
+                    GUI.color = icon.ValidColor;
+                    GUI.DrawTexture (rect, _folderIconBack);
+                    if (icon.OverlayIcon != null) {
+                        GUI.color = savedColor;
+                        var tex = EditorGUIUtility.Load (icon.OverlayIcon) as Texture2D;
+                        if ((System.Object) tex != null) {
+                            rect.width *= 0.5f;
+                            rect.height *= 0.5f;
+                            rect.x += rect.width;
+                            rect.y += rect.height;
+                            GUI.DrawTexture (rect, tex);
+                        }
+                    } else {
+                        GUI.color = savedColor;
+                    }
+                }
+            }
+        }
+
+        static FolderIconDesc GetCustomIcon (string guid) {
+            return _allDescs.ContainsKey (guid) ? _allDescs[guid] : null;
+        }
+
+        [MenuItem ("Assets/LeopotamGroup/FolderIcons/Open editor...", false, 1)]
+        static void OpenUI () {
+            var path = AssetDatabase.GetAssetPath (Selection.activeObject);
+            if (!string.IsNullOrEmpty (path) && AssetDatabase.IsValidFolder (path)) {
+                var win = GetWindow<FolderIconEditor> ();
+                win.Init (path);
+                win.ShowUtility ();
+            } else {
+                EditorUtility.DisplayDialog (Title, "Select valid folder first", "Close");
+            }
+        }
+
+        [MenuItem ("Assets/LeopotamGroup/FolderIcons/Reset all settings", false, 100)]
+        static void ClearAllUI () {
+            _allDescs.Clear ();
+            SaveInfo ();
+            EditorUtility.DisplayDialog (Title, "Successfully reset", "Close");
+        }
+
+        void Init (string folderPath) {
+            _folderPath = folderPath;
+            _folderGuid = AssetDatabase.AssetPathToGUID (_folderPath);
+            _folderDesc = GetCustomIcon (_folderGuid) ?? new FolderIconDesc ();
+        }
+
+        void OnGUI () {
+            if (_folderDesc == null) {
+                Close ();
+                return;
+            }
+            var needToSave = false;
+            var color = _folderDesc.ValidColor;
+            var newR = (byte) EditorGUILayout.IntSlider ("Color R", color.r, 0, 255);
+            var newG = (byte) EditorGUILayout.IntSlider ("Color G", color.g, 0, 255);
+            var newB = (byte) EditorGUILayout.IntSlider ("Color B", color.b, 0, 255);
+            needToSave |= newR != color.r || newG != color.g || newB != color.b;
+
+            var iconName = _folderDesc.OverlayIcon ?? "";
+            var newIconName = EditorGUILayout.TextField ("Folder Overlay icon", iconName);
+            needToSave |= string.CompareOrdinal (newIconName, iconName) != 0;
+
+            if (needToSave) {
+                _folderDesc.ValidColor = new Color32 (newR, newG, newB, 255);
+                _folderDesc.OverlayIcon = newIconName.Length > 0 ? newIconName : null;
+                if (_folderDesc != GetCustomIcon (_folderGuid)) {
+                    _allDescs[_folderGuid] = _folderDesc;
+                }
+                EditorApplication.RepaintProjectWindow ();
+            }
+            if (GUILayout.Button ("Reset for this folder")) {
+                if (GetCustomIcon (_folderGuid) != null) {
+                    _allDescs.Remove (_folderGuid);
+                }
+                OnLostFocus ();
+            }
+        }
+
+        void OnLostFocus () {
+            SaveInfo ();
+            Close ();
+        }
+    }
+}
