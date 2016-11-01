d80e9e56073e041355d6198fd3dc25df4f05eca7
Leopotam <leopotam@gmail.com>
2016-11-01 16:16:06 +0300

    [+] SystemUI: Localization for Text component.

---
+++ Assets/LeopotamGroup/SystemUI/Localization/TextLocalization.cs
@@ -0,0 +1,34 @@
+﻿//-------------------------------------------------------
+// LeopotamGroupLibrary for unity3d
+// Copyright (c) 2012-2016 Leopotam <leopotam@gmail.com>
+//-------------------------------------------------------
+
+using LeopotamGroup.Localization;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LeopotamGroup.SystemUI.Localization {
+    /// <summary>
+    /// Localization helper for System UI Text.
+    /// </summary>
+    [RequireComponent (typeof (Text))]
+    public sealed class TextLocalization : MonoBehaviour {
+        [SerializeField]
+        string _token;
+
+        Text _text;
+
+        void OnEnable () {
+            OnLocalize ();
+        }
+
+        void OnLocalize () {
+            if (!string.IsNullOrEmpty (_token)) {
+                if ((System.Object) _text == null) {
+                    _text = GetComponent <Text> ();
+                }
+                _text.text = Localizer.Get (_token);
+            }
+        }
+    }
+}
