2a58d23c25d48c377a2b356880f63013cac46526
Leopotam <leopotam@gmail.com>
2017-05-03 17:22:51 +0300

    * Math: GetHashCode fixes for Vector2i / Vector3i / Vector4i types.

--- Math/Vector2i.cs
+++ Math/Vector2i.cs
@@ -124,7 +124,7 @@ namespace LeopotamGroup.Math {
         /// Get hash code.
         /// </summary>
         public override int GetHashCode () {
-            return base.GetHashCode ();
+            return x.GetHashCode () ^ y.GetHashCode () << 2;
         }
 
         /// <summary>
