5cb43f618fe8eb3ba047e6984f91d19027a6e864
rhanza <rhanza@bk.ru>
2018-03-11 02:10:27 +0300

    * Fix by Leopotam review (https://github.com/Leopotam/LeopotamGroupLibraryUnity/pull/5#pullrequestreview-102865485) * Fix format style

--- Common/UnityExtensions.cs
+++ Common/UnityExtensions.cs
@@ -47,7 +47,7 @@ namespace LeopotamGroup.Common {
         /// <param name="target">Root of search.</param>
         /// <param name="name">Name to search.</param>
         public static Transform FindRecursive (this Transform target, string name) {
-            if (target == null || string.CompareOrdinal (target.name, name) == 0) {
+            if ((object) target == null || string.CompareOrdinal (target.name, name) == 0) {
                 return target;
             }
             Transform retVal = null;
@@ -66,18 +66,14 @@ namespace LeopotamGroup.Common {
         /// <returns>Transform of found GameObject.</returns>
         /// <param name="target">Root of search.</param>
         /// <param name="tag">Tag to search.</param>
-        public static Transform FindRecursiveByTag(this Transform target, string tag)
-        {
-            if (target == null || string.CompareOrdinal(target.tag, tag) == 0)
-            {
+        public static Transform FindRecursiveByTag(this Transform target, string tag) {
+            if ((object) target == null || target.CompareTag(tag)) {
                 return target;
             }
             Transform retVal = null;
-            for (var i = target.childCount - 1; i >= 0; i--)
-            {
+            for (var i = target.childCount - 1; i >= 0; i--) {
                 retVal = target.GetChild(i).FindRecursiveByTag(tag);
-                if (retVal != null)
-                {
+                if (retVal != null) {
                     break;
                 }
             }
